b5855ba1276e610c5d34047bfd1029d33635300b
noushad970 <ug2102038@cse.pstu.ac.bd>
2024-12-17 19:14:11 +0900

    connecting to stun server and open my video audio

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,28 @@
 const createUserBtn=document.getElementById("create-user");
 const username=document.getElementById("username");
 const socket= io();
+const allUsersHtml= document.getElementById("allusers");
+const PeerConnection=(function(){
+    let peerConnection;
+    const createPeerConnection=()=>{
+        const confg={
+            iceServers:[
+                {
+                    urls:'stun:stun.l.google.com:19302'
+                }
+            ]
+        };
+        peerConnection=new RTCPeerConnection(confg);
+    }
+    return {
+        getInstance:()=>{
+            if(!peerConnection){
+                peerConnection=createPeerConnection();
+            }
+            return peerConnection;
+        }
+    }
+})()
 // handle browser events
 createUserBtn.addEventListener("click",(e)=>{
     if(username.value!=="")
@@ -13,5 +35,42 @@ createUserBtn.addEventListener("click",(e)=>{
 
 // handle socket events
 socket.on("joined",allUsers=>{
+    
     console.log("All Users: ",allUsers);
+    const createUsershtml=()=>{
+        allUsersHtml.innerHTML="";
+        for(const user in allUsers){
+            const li=document.createElement("li");
+            li.textContent=`${user} ${user=== username.value? "(You)":""}`
+        if(user!==username.value){
+            const button=document.createElement("button");
+            button.classList.add("call-btn");
+            button.addEventListener("click",(e)=>{
+                startCall(user);
+            })
+            const img= document.createElement("img");
+            img.setAttribute("src","/images/phone.png");
+            img.setAttribute("width",20);
+            button.appendChild(img);
+            li.appendChild(button);
+        }
+        allUsersHtml.appendChild(li);
+        }
+    }
+    createUsershtml();
 })
+startCallMethod=(user)=>{
+console.log("User in Call: ",user);
+}
+
+//initialize app
+const startMyVideo=async()=>{
+    try{
+        const stream= await navigator.mediaDevices.getUserMedia({audio:true,video:true});
+        console.log({stream});
+    }catch(e){
+        console.log("Errors: ",console.error());
+
+    }
+}
+startMyVideo();
